15c86df15bcbd23fa496750879a6ca35412646d8
WoringElf428 <39079495+WoringElf428@users.noreply.github.com>
2025-01-12 23:56:24 +0900

    スコア表示など追加, ussの変更反映

--- Assets/UI component/UImanager.cs
+++ Assets/UI component/UImanager.cs
@@ -5,26 +5,75 @@ using UnityEngine.UIElements;
 
 public class UIController : MonoBehaviour
 {
-    private AudioAnalyzer analyzer;
-    private GameManager manager; 
+    [SerializeField] private AudioAnalyzer analyzer;
+    [SerializeField] private GameManager manager;
     private VisualElement root;
 
+    //動的なUIパーツ
+    Button recordBtn;
+    Button munuBtn;
+
+    Label recordLabel;
+    Label scoreLabel;
     void Start()
     {
         analyzer = GameObject.Find("AudioAnalyzer").GetComponent<AudioAnalyzer>();
         manager = GameManager.Instance;
 
         root = this.GetComponent<UIDocument>().rootVisualElement;
+
         //ボタンイベントの登録
-        var recordBtn = root.Q<Button>("RecordButton"); 
-        if (recordBtn is not null) recordBtn.clicked += () => { /* クリック時の処理 */ };        
-        var munuBtn = root.Q<Button>("Button1");
+        recordBtn = root.Q<Button>("RecordButton");
+        if (recordBtn is not null) recordBtn.clicked += () => { recordStart(); };
+        munuBtn = root.Q<Button>("Button1");
         if (munuBtn is not null) munuBtn.clicked += () => { /* クリック時の処理 */ };
+
+        //動的なコンポーネントの取得
+        scoreLabel = root.Q<Label>("ScoreLabel");
+        recordLabel = root.Q<Label>("RecordLabel");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    /* 
+     * レコードボタンが押されたときの処理
+     */
+    void recordStart()
+    {
+        Debug.Log("レコードスタート");
+
+        // RecordButtonとRecordLabelのUI変更
+        if (recordBtn != null && recordLabel != null)
+        {
+            recordBtn.AddToClassList("#RecordingButton:active"); // ボタンスタイル変更
+            recordLabel.AddToClassList("#RecordingLabel:active"); // ラベルスタイル変更
+            recordLabel.text = "Now Recording...";
+        }
+
+        //レコードの開始処理
+        if (analyzer is not null)
+        {
+            //analyzer.StartAnalyzer();
+        }
+        else
+        {
+            Debug.LogError("analzerが指定されてない");
+        }
+        //デバック
+        //displayScore(1000);
+    }
+
+    void displayInput()
+    {
+
+    }
+
+    public void displayScore(int text)
+    {
+        scoreLabel.text = text.ToString("D6");
     }
 }
